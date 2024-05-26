639aaef411bf9f99b3b3669494ac648e7eb07a56
alisahin <alisahin10@icloud.com>
2024-05-27 00:08:30 +0300

    authentication, router, JWT, handlers created and tested

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"Chat_App/db"
+	"Chat_App/internal/user"
+	"Chat_App/router"
 	"log"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("Error connecting to database: %s", err)
 	}
+
+	userRep := user.NewRepository(dbConn.GetDB())
+	user.NewService(userRep)
+	userSvc := user.NewService(userRep)
+	userHandler := user.NewHandler(userSvc)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8080")
 }
---
+++ server/internal/user/user_repository.go
@@ -0,0 +1,48 @@
+package user
+
+import (
+	"context"
+	"database/sql"
+)
+
+type DBTX interface {
+	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
+	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
+}
+
+type repository struct {
+	db DBTX
+}
+
+func NewRepository(db DBTX) Repository {
+	return &repository{db: db}
+}
+
+func (r *repository) CreateUser(ctx context.Context, user *User) (*User, error) {
+	var lastInsertId int
+	query := "INSERT INTO users(username, password, email) VALUES($1, $2, $3) returning id"
+	err := r.db.QueryRowContext(ctx, query, user.Username, user.Password, user.Email).Scan(&lastInsertId)
+	if err != nil {
+		return nil, err
+	}
+
+	user.ID = int64(lastInsertId)
+	return user, nil
+}
+
+func (r *repository) GetUserByEmail(ctx context.Context, email string) (*User, error) {
+	var u User
+
+	query := "SELECT id, email, username, password FROM users WHERE email = $1"
+	err := r.db.QueryRowContext(ctx, query, email).Scan(&u.ID, &u.Email, &u.Username, &u.Password)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil // No user found, return nil
+		}
+		return nil, err // Some other error occurred
+	}
+
+	return &u, nil
+}
